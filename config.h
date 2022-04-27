530f0429bcfce18c8014be22bb0d974cd13fecdb
Quang <godyudodis@gmail.com>
2022-04-28 02:00:15 +0700

    Show topbar

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static unsigned int gappoh    = 3;       /* horiz outer gap between windows and 
 static unsigned int gappov    = 3;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 0;        /* 0 means no bar */
+static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "SauceCodePro Nerd Font:pixelsize=16:antialias=true:autohint=true", "SauceCodePro Nerd Font:pixelsize=16:antialias=true:autohint=true" };
 static char normbgcolor[]           = "#1e222a";
@@ -174,9 +174,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_i,		setlayout,	{.v = &layouts[7]} }, /* centeredfloatingmaster */
 	{ MODKEY,			XK_o,		incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,		XK_o,		incnmaster,     {.i = -1 } },
-	{ MODKEY,		    XK_p,			spawn,		SHCMD("spotifyctl -q playpause") },
-	{ MODKEY,			XK_bracketleft,		spawn,		SHCMD("spotifyctl -q previous") },
-	{ MODKEY,			XK_bracketright,	spawn,		SHCMD("spotifyctl -q next") },
+	{ MODKEY,		    XK_p,			spawn,		SHCMD("obsidian") },
+	// { MODKEY,			XK_bracketleft,		spawn,		SHCMD("spotifyctl -q previous") },
+	// { MODKEY,			XK_bracketright,	spawn,		SHCMD("spotifyctl -q next") },
 	{ MODKEY,			XK_backslash,		view,		{0} },
 	/* { MODKEY|ShiftMask,		XK_backslash,		spawn,		SHCMD("") }, */
 
